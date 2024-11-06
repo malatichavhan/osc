24ab16ea83b40c6f3a82bd1865f69e2e1dcc4ddd
Malati <malati.a.chavhan@gmail.com>
2024-11-07 12:18:16 +1300

    Update database.js

--- helper/database.js
+++ helper/database.js
@@ -1,12 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST
+const sequelize = new Sequelize(process.env.DB_NAME || 'nodejs', process.env.DB_USER || 'nodejs' , process.env.DB_PASSWORD || 'nodejs',{
+    dialect: process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST || 'localhost'
 });
 
 module.exports = sequelize;
-
-
-
-
