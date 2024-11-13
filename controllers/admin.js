11f0feec38557a2f107c7522852840ae2e8709dc
Malati Purushottam Rathod <rath95@manukaumail.com>
2024-11-13 19:18:38 +1300

    Authentication

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-      isAuthenticated: req.isLoggedIn,
+      isAuthenticated: req.session.IsLoggedIn
   });
 };
 
@@ -55,7 +55,7 @@ exports.getEditProduct = (req, res, next) => {
           path: '/admin/edit-product',
           editing: editMode,
           product: product,
-            isAuthenticated: req.isLoggedIn,
+            isAuthenticated: req.session.IsLoggedIn
         });
       })
       .catch(err => console.log(err));
@@ -90,8 +90,8 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.isLoggedIn,
-            i
+            isAuthenticated: req.session.IsLoggedIn
+
       });
   })
       .catch(err => console.log(err));
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,80 @@
+const User = require("../models/user");
+const bcrypt  = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn=req
-       .get('Cookie').
-        split(';')[1]
-        .trim()
-        .split('=')[1]===true;
             res.render('auth/login', {
                 path: '/login',
                 pageTitle: 'Login',
-                isAuthenticated: false
+                isAuthenticated: req.session.IsLoggedIn
             });
 };
 
 exports.postLogin= (req, res, next) => {
-        res.setHeader('Set-Cookie', 'loggedIn=true; httpOnly');
-        res.redirect('/');
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ where: { email: email} }, (err, user) => {
+        if (err) {console.log(err);}
+        return res.redirect('/login');
+    }).then(user => {
+        if (!user) {
+            return res.redirect('/login');
+        }
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) {return res.redirect('/login');}
+            if (isMatch) {
+                req.session.IsLoggedIn = true;
+                req.session.user = user;
+                req.session.save((err) => {
+                    console.log(err);
+                    res.redirect('/');
+                });
+            }else{
+                return res.redirect('/login');
+            }
+        });
+
+
+
+    })
+
+
+};
+
+exports.postLogout= (req, res, next) => {
+    req.session.destroy((err) =>{
+        console.log(err);
+        res.redirect('/')
+    });
+};
+
+exports.getSignup= (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    })
+};
+
+exports.postSignup= (req, res, next) => {
+ const email = req.body.email;
+ const password = req.body.password;
+ const confirmPassword = req.body.confirmPassword;
+
+
+ User.findOne({ where: { email: email } }, (err, user) => {
+     if (err) {console.log(err);}
+ }).then(user =>{
+     if (user) {
+         return res.redirect('/signup');
+     }
+     bcrypt.hash(password,12).then(hash => {
+         User.create({password: hash, email: email, cart: {cartItems: []}});
+         res.redirect('/login');
+     })
+
+ });
+
+
 };
 
