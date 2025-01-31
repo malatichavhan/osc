25a9adac45ec0dc1ca0461757399dd7b97fe1774
Malati Purushottam Rathod <rath95@manukaumail.com>
2025-01-31 22:20:48 +1300

    immplimented for forget password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const Cart = require("../models/cart");
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'malatirathod1111@gmail.com',
+        pass: 'gejv gkbl srdk yvei'
+    }
+});
 
 exports.getLogin = (req, res, next) => {
     let massage =req.flash('error');
@@ -30,7 +39,7 @@ exports.postLogin = (req, res, next) => {
             return res.redirect('/login');
         }
         bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (err) { return res.redirect('/login'); }
+                if (err) { return res.redirect('/login'); }
             if (isMatch) {
                 req.session.IsLoggedIn = true;
                 req.session.user = user;
@@ -85,3 +94,98 @@ exports.postSignup = (req, res, next) => {
     });
 
 };
+
+exports.getForgetPassword = (req, res, next) => {
+    const email = req.body.email;
+    let massage =req.flash('error');
+    if(massage.length > 0){
+        massage = massage[0];
+    }else {
+        massage = null;
+    }
+    res.render('auth/forgetPassword', {errorMassage: massage})
+};
+
+exports.postSendEmail = (req, res, next) => {
+    const email = req.body.email;
+
+    User.findOne({ where: { email: email } }, (err, user) => {
+        if (err) { console.log(err); }
+        req.flash('error', 'Your account does not exist.');
+        return res.redirect('/forgetpassword');
+    }).then(user => {
+        if (!user) {
+            req.flash('error', 'Your account does not exist.');
+            return res.redirect('/forgetpassword');
+        }else {
+            bcrypt.hash(email, user.id).then(hash => {
+                //Sending email
+            user.resetPasswordHash=hash;
+            user.save();
+                const mailOptions = {
+                    from: 'malatirathod1111@gmail.com',
+                    to: email,
+                    subject: 'Password Reset For themalati.com',
+                    text: 'Click the link to reset password https://themalati.com/resetpassword/'+hash.replaceAll("/","%2F")
+                };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+                //Sending email finished
+            });
+
+            res.render('auth/sendEmail', {})
+        }
+    });
+
+
+};
+
+exports.getResetPassword = (req, res, next) => {
+    const hash = req.params.hash;
+    User.findOne({ where: { resetPasswordHash: hash } }, (err, user) => {
+        if (err) { console.log(err); }
+        req.flash('error', 'Invalid Or expired link for password reset.');
+        return res.redirect('/forgetpassword');
+    }).then(user => {
+        if (!user) {
+            req.flash('error', 'Invalid Or expired link for password reset.');
+            return res.redirect('/forgetpassword');
+        }else {
+            res.render('auth/ResetPassword', {hash:hash});
+        }
+    });
+
+};
+
+exports.postUpdatepassword = (req, res, next) => {
+    const password = req.body.password;
+    const hash = req.body.hash;
+    User.findOne({ where: { resetPasswordHash: hash } }, (err, user) => {
+        if (err) { console.log(err); }
+        req.flash('error', 'Invalid Or expired link for password reset.');
+        return res.redirect('/forgetpassword');
+    }).then(user => {
+        if (!user) {
+            req.flash('error', 'Invalid Or expired link for password reset.');
+            return res.redirect('/forgetpassword');
+        }else {
+
+            bcrypt.hash(password, 12).then(hash => {
+                user.resetPasswordHash=null;
+                user.password=hash;
+                user.save();
+                res.redirect('/login')
+            });
+
+
+        }
+    });
+
+
+};
+
--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,10 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout)
 router.post('/signup', authController.postSignup);
 router.get('/signup', authController.getSignup);
+router.get('/forgetpassword', authController.getForgetPassword);
+router.post('/sendemail', authController.postSendEmail);
+router.get('/resetpassword/:hash', authController.getResetPassword);
+router.post('/updatepassword',authController.postUpdatepassword);
 
 
 
